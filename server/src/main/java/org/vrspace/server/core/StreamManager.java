251893bef30e7365a4e0e83e1717a0071f94cb76
Asif Bilgrami <asif.bilgrami@systemsltd.com>
2023-01-06 14:57:30 +0500

    Merge local changes

--- server/src/main/java/org/vrspace/server/core/StreamManager.java
+++ server/src/main/java/org/vrspace/server/core/StreamManager.java
@@ -92,7 +92,8 @@ public class StreamManager {
   public void disconnect(Client client) throws OpenViduException {
     // client is only connected if it has session token
     if (client.getToken(serviceId) != null && client.getWorld() != null) {
-      String name =client.getWorld().getName();
+      //Client user1 = sessionManager.getClient(client.getId());
+      String name ="classroom"; //client.getWorld().getName();
       Session session = sessions.get(name);
       log.info("Name is in disconnect " + name);
       if (session != null) {
