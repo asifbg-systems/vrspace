7541140699afa29d1a2a79305e6a2c8663745004
mammar.zafar@systemsltd.com <mammar.zafar@systemsltd.com>
2023-01-12 17:04:32 +0500

    ShareScreen Repositioning

--- content/worlds/HOF/world.js
+++ content/worlds/HOF/world.js
@@ -174,7 +174,7 @@ export class Classroom extends World {
   // executed once connected to the server and entered the space
   entered(welcome) {
     this.screencast = new Screencast(this, 'teacher');
-    this.screencast.screenShareMesh.position = new BABYLON.Vector3(-0.06, 2, -17);
+    this.screencast.screenShareMesh.position = new BABYLON.Vector3(0.06, 1.5, -17);
     this.screencast.screenShareMesh.rotation = new BABYLON.Vector3(0, Math.PI, 0);
     this.screencast.videoMesh.position = new BABYLON.Vector3(0, 1.5, -17);
     this.screencast.videoMesh.rotation = new BABYLON.Vector3(0, Math.PI, 0);
